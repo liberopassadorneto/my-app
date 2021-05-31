42490bc8b1bcde6ed5bccf966d99e7b27851042a
libero <libero@griks.com.br>
2021-05-31 11:04:21 -0400

    homepage

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-import "./App.css";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import Home from "./pages/HomePage";
-import SingIn from "./pages/SingInPage";
-import ThankYou from "./pages/ThankYouPage";
+import './App.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Home from './pages/HomePage';
+import SingIn from './pages/SingInPage';
+import ThankYou from './pages/ThankYouPage';
 
 function App() {
   return (
     <Router>
       <Switch>
-        <Route path="/" exact component={Home} />
+        <Route path="/my-app" exact component={Home} />
         <Route path="/singin" component={SingIn} />
         <Route path="/thankyou" component={ThankYou} />
       </Switch>
