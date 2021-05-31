935fb729ed898891624f66e8676c29f4f899b08b
libero <libero@griks.com.br>
2021-05-31 11:43:52 -0400

    spa

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { HashRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Home from './pages/HomePage';
 import SingIn from './pages/SingInPage';
 import ThankYou from './pages/ThankYouPage';
@@ -9,8 +9,8 @@ function App() {
     <Router basename={process.env.PUBLIC_URL}>
       <Switch>
         <Route exact path="/" component={Home} />
-        <Route exact path="/singin" component={SingIn} />
-        <Route exact path="/thankyou" component={ThankYou} />
+        <Route path="/singin" component={SingIn} />
+        <Route path="/thankyou" component={ThankYou} />
         <Route component={Home} />
       </Switch>
     </Router>
